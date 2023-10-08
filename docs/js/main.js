03abba505663a6bb30e4419f45eeb03808d58f1b
Matthieu Baralle <32303868+mbaralle@users.noreply.github.com>
2023-10-08 22:54:24 +0200

    add full-screen, next and previous toggles

--- docs/js/main.js
+++ docs/js/main.js
@@ -12,3 +12,60 @@ function menuClick() {
   }
   isMenuOpened = !isMenuOpened;
 }
+
+function fullScreenToggle() {
+  const toggle = document.createElement("span");
+  toggle.title = "Toggle full screen";
+  toggle.classList.add("toggle");
+  toggle.classList.add("full-screen-toggle");
+  toggle.addEventListener("click", (event) => {
+    if (event.target.parentElement.classList.contains("full-screen")) {
+      event.target.parentElement.classList.remove("full-screen");
+      document.body.classList.remove("no-scroll");
+    } else {
+      event.target.parentElement.classList.add("full-screen");
+      document.body.classList.add("no-scroll");
+    }
+  });
+
+  return toggle;
+}
+
+function artSibling(art, direction) {
+  const method = `${direction}ElementSibling`;
+  for (let sibling of [
+    art[method]?.children[0],
+    art[method],
+    art.parentElement[method],
+  ]) {
+    while (sibling) {
+      if (sibling.classList.contains("art")) {
+        return sibling;
+      }
+      sibling = sibling[method];
+    }
+  }
+}
+
+function addSiblingToggle(art, direction) {
+  const sibling = artSibling(art, direction);
+  if (sibling) {
+    const toggle = document.createElement("span");
+    toggle.title = direction.charAt(0).toUpperCase() + direction.slice(1);
+    toggle.classList.add(`toggle`);
+    toggle.classList.add(`${direction}-toggle`);
+    toggle.addEventListener("click", () => {
+      art.getElementsByClassName("full-screen-toggle")[0].click();
+      sibling.getElementsByClassName("full-screen-toggle")[0].click();
+    });
+
+    art.appendChild(toggle);
+  }
+}
+
+const arts = document.getElementsByClassName("art");
+for (const art of arts) {
+  art.appendChild(fullScreenToggle());
+  addSiblingToggle(art, "previous");
+  addSiblingToggle(art, "next");
+}
